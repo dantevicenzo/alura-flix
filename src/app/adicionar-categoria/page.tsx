134b73b0e70aecf93fb7de65eacfab7f8583443c
Dante Vicenzo <dantevcz@gmail.com>
2023-08-09 18:42:53 -0300

    add new category page

---
+++ src/app/adicionar-categoria/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+const AdicionarCategoria = () => {
+  return (
+    <main className={styles.main}>
+      <h1>Adicionar Categoria</h1>
+    </main>
+  )
+}
+
+export default AdicionarCategoria
