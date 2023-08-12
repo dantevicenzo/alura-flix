50ee667a5edd2bd4368f26dde47bc02d8f76a976
Dante Vicenzo <dantevcz@gmail.com>
2023-08-11 23:35:53 -0300

    Footer logo image is now a link to home page

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image'
 import styles from './footer.module.css'
 import logo from 'public/logo.png'
+import Link from 'next/link'
 
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <Image src={logo} height={60} alt="" />
+      <Link href="/">
+        <Image src={logo} height={60} alt="" />
+      </Link>
     </footer>
   )
 }
