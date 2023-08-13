0d5da527c1d36b0211ec370818d3d781582992ba
Dante Vicenzo <dantevcz@gmail.com>
2023-08-13 06:14:26 -0300

    Button visibility logic

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,21 +1,32 @@
+'use client'
+
 import Image from 'next/image'
 import { Button } from './button'
 import logo from 'public/logo.png'
+import { usePathname } from 'next/navigation'
 
 import styles from './header.module.css'
 import Link from 'next/link'
 
 export const Header = () => {
+  const pathname = usePathname()
+  const isHomePage = pathname === '/'
+  const isCategoryPage =
+    pathname === '/adicionar-categoria' || pathname === '/editar-categoria'
+  const showNewVideoButton = isHomePage || isCategoryPage
+
   return (
     <header className={styles.header}>
       <Link href="/">
         <Image src={logo} alt="" height={40} />
       </Link>
-      <Link href="/adicionar-video">
-        <Button variantSize="sm" variantColor="dark">
-          Novo vídeo
-        </Button>
-      </Link>
+      {showNewVideoButton && (
+        <Link href="/adicionar-video">
+          <Button variantSize="sm" variantColor="dark">
+            Novo vídeo
+          </Button>
+        </Link>
+      )}
     </header>
   )
 }
